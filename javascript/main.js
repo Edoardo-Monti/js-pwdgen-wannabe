dfbdc226f0462dedc1a6484dee3bb1717e24ed85
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-21 14:47:29 +0100

    inizio js

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+/* 
+
+Chiedi all’utente il suo nome,
+    -creare la variabile nome e prompt per chiedere
+poi chiedi il suo cognome,
+    -creare la variabile cognonome e prompt per chiedere
+poi chiedi il suo colore preferito
+    -creare la variabile colore preferito e prompt per chiedere
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+*/
+
