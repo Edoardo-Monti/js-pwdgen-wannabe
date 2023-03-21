4aab07eb6ce66c9c3221f359ae0eff065580522b
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-21 15:08:40 +0100

    creazione pasword

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,18 @@ poi chiedi il suo cognome,
     -creare la variabile cognonome e prompt per chiedere
 poi chiedi il suo colore preferito
     -creare la variabile colore preferito e prompt per chiedere
+
 Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 */
 
+const nome = prompt(`come ti chiami?`);
+const cognome = prompt(`qual'e il tuo cogmome?`);
+const color = prompt(`qual'e il tuo colore preferito?`);
+
+let password = nome + cognome + color + 21;
+
+console.log(password);
+
+
+document.getElementById(`text`) . innerText =`${password}`;
